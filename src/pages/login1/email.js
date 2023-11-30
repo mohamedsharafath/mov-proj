5eb2ac7acd93e5786882454de1bc0dd8884824c7
mohamedsharafath <sharafath0sms@gmail.com>
2023-11-30 09:39:11 +0530

    login page updated

--- src/pages/login1/email.js
+++ src/pages/login1/email.js
@@ -1,36 +1,83 @@
 import React, { useState } from "react";
 import './email.css'
 import App from "../../App";
+import { TextField, Button } from "@mui/material";
 
 export const Login = () => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [done, setDone] = useState(false);
+    const [eerror,seteerror] = useState(false)
+    const [ehelpertext,setehelpertext] = useState('')
+    const [perror,setperror] = useState(false)
+    const [phelpertext,setphelpertext] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if(email==='')
+        {
+            seteerror(true)
+            setehelpertext('Enter your email')
+        }
+        if(pass==='')
+        {
+            setperror(true)
+            setphelpertext('Enter your Password')
+        }
+        else
         setDone(true)
-        // alert("hello");
-        // <App/>
-        // console.log(email);
+        
+       
     }
 
     return (
         <>
+        {done ? (<App/>) :
         <div className="App1">
         <div className="auth-form-container">
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Log In</button>
+                <TextField 
+                name="Email"
+                value={email}
+                onChange={(e) => {
+                    setEmail(e.target.value)
+                    seteerror(false)
+                    setehelpertext('')
+                }
+                }
+                variant="outlined"
+                type="email"
+                placeholder="youremail@gmail.com"
+                label="Email"
+                error={eerror}
+                helperText={ehelpertext} 
+                /> 
+                <TextField 
+                name="Password"
+                value={pass}
+                onChange={(e) => {
+                    setPass(e.target.value)
+                    setperror(false)
+                    setphelpertext('')
+                }
+                }
+                variant="outlined"
+                type="password"
+                placeholder="*******"
+                label="Password"
+                error={perror}
+                helperText={phelpertext} 
+                /> 
+                {/* <label htmlFor="email">Email</label>
+                <input value={email} onChange={(e) => {setEmail(e.target.value)}}type="email" placeholder="youremail@gmail.com" id="email" name="Email" />
+                <label htmlFor="password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="Password"  /> */}
+                <Button type="submit" variant="contained">Log In</Button>
             </form>
             {/* <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button> */}
         </div>
-        </div>
-        {done ? <App/> : null}
+        </div>}
         </> 
     )
 }
